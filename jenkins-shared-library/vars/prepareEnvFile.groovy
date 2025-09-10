046ae828074ba98bd98aaabbb6cf4f256be5c75c
Rehan Shaikh <ifrit1207@gmail.com>
2025-09-10 06:21:23 -0700

    Update prepareEnvFile.groovy

--- jenkins-shared-library/vars/prepareEnvFile.groovy
+++ jenkins-shared-library/vars/prepareEnvFile.groovy
@@ -2,17 +2,20 @@ def call() {
     withCredentials([
         string(credentialsId: 'mysql-root-password', variable: 'MYSQL_ROOT_PASSWORD'),
         string(credentialsId: 'jwt-secret', variable: 'JWT_SECRET'),
-        string(credentialsId: 'db-password', variable: 'DB_PASSWORD') // optional, if used
+        string(credentialsId: 'db-password', variable: 'DB_PASSWORD') 
     ]) {
         writeFile file: '.env', text: """
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 MYSQL_DATABASE=chattingo_db
 SPRING_DATASOURCE_URL=jdbc:mysql://dbservice:3306/chattingo_db?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true
 SPRING_DATASOURCE_USERNAME=root
-SPRING_DATASOURCE_PASSWORD=${MYSQL_ROOT_PASSWORD}  // or ${DB_PASSWORD} if different
+SPRING_DATASOURCE_PASSWORD=${DB_PASSWORD} 
 JWT_SECRET=${JWT_SECRET}
 CORS_ALLOWED_ORIGINS=https://cloudwithrehan.in,https://www.cloudwithrehan.in
+CORS_ALLOWED_METHODS=GET,POST,PUT,DELETE,OPTIONS
+CORS_ALLOWED_HEADERS=*
 SPRING_PROFILES_ACTIVE=production
+SERVER_PORT=8080
 """
     }
 }
