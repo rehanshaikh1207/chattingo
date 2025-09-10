070e30eb4879b27bcac63d403ee63e0db9e8d1f4
Rehan Shaikh <ifrit1207@gmail.com>
2025-09-10 07:39:32 -0700

    Update prepareEnvFile.groovy

--- jenkins-shared-library/vars/prepareEnvFile.groovy
+++ jenkins-shared-library/vars/prepareEnvFile.groovy
@@ -2,14 +2,14 @@ def call() {
     withCredentials([
         string(credentialsId: 'mysql-root-password', variable: 'MYSQL_ROOT_PASSWORD'),
         string(credentialsId: 'jwt-secret', variable: 'JWT_SECRET'),
-        string(credentialsId: 'db-password', variable: 'DB_PASSWORD') 
+        string(credentialsId: 'db-password', variable: 'DB_PASSWORD')
     ]) {
-        writeFile file: '.env', text: """
+        def envContent = """
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 MYSQL_DATABASE=chattingo_db
 SPRING_DATASOURCE_URL=jdbc:mysql://dbservice:3306/chattingo_db?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true
 SPRING_DATASOURCE_USERNAME=root
-SPRING_DATASOURCE_PASSWORD=${DB_PASSWORD} 
+SPRING_DATASOURCE_PASSWORD=${DB_PASSWORD}
 JWT_SECRET=${JWT_SECRET}
 CORS_ALLOWED_ORIGINS=https://cloudwithrehan.in,https://www.cloudwithrehan.in
 CORS_ALLOWED_METHODS=GET,POST,PUT,DELETE,OPTIONS
@@ -17,7 +17,8 @@ CORS_ALLOWED_HEADERS=*
 SPRING_PROFILES_ACTIVE=production
 SERVER_PORT=8080
 """.stripIndent().trim()
-writeFile file: '.env', text: envContent
-echo '.env file created in workspace'
+
+        writeFile file: '.env', text: envContent
+        echo '.env file created in workspace'
     }
 }
