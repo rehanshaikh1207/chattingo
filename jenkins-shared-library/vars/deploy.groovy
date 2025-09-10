6fd2109ebd8fb021ff4a783f7a597b5ab5caee8a
Rehan Shaikh <ifrit1207@gmail.com>
2025-09-10 07:30:51 -0700

    Update deploy.groovy

--- jenkins-shared-library/vars/deploy.groovy
+++ jenkins-shared-library/vars/deploy.groovy
@@ -2,6 +2,7 @@ def call() {
     stage('Deploy') {
         echo "Deploying to VPS..."
         sh """
+            cd $WORKSPACE
             docker-compose -f docker-compose.prod.yml down || true
             docker-compose -f docker-compose.prod.yml up -d
         """
